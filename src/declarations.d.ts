cde0d1eced4ba739c9e141e45a9f32f2511b7267
Kamil Mathea <kamilmathe@gmail.com>
2026-08-13 14:37:29 +0200

    first commit

---
+++ src/declarations.d.ts
@@ -0,0 +1,10 @@
+/**
+ * VITE ENVIRONMENT TYPINGS
+ * 
+ * Nicht direkt Bestandteil des DA-Lehrplans:
+ * Diese Datei lädt die Typ-Definitionen von Vite. Sie signalisiert TypeScript,
+ * dass Nicht-Code-Dateien (wie .scss, .css oder Bilder) in .ts-Dateien 
+ * importiert werden dürfen, ohne dass VS Code einen Syntax-Fehler anzeigt.
+ */
+
+/// <reference types="vite/client" />
